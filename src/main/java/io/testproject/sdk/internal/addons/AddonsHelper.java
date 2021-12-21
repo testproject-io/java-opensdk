176ec701dbc1e560d8d4afa531350446b098a7f7
shai glatshtein <shai.glatshtein@testproject.io>
2021-12-21 16:53:14 +0200

    feat: allow running the new actions using AddonsHelper
    Signed-off-by: shai glatshtein <shai.glatshtein@testproject.io>

--- src/main/java/io/testproject/sdk/internal/addons/AddonsHelper.java
+++ src/main/java/io/testproject/sdk/internal/addons/AddonsHelper.java
@@ -17,61 +17,81 @@
 
 package io.testproject.sdk.internal.addons;
 
+import io.testproject.sdk.drivers.ReportingDriver;
+import io.testproject.sdk.internal.addons.interfaces.Action;
+import io.testproject.sdk.internal.addons.interfaces.ElementAction;
 import io.testproject.sdk.internal.rest.AgentClient;
-import io.testproject.sdk.internal.rest.messages.ActionExecutionResponse;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriverException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.openqa.selenium.InvalidArgumentException;
+import org.openqa.selenium.WebDriver;
 
-import java.lang.reflect.Field;
-import java.util.Arrays;
-import java.util.Optional;
+import java.lang.reflect.ParameterizedType;
 
 /**
  * Helper class allowing to execute Addons.
+ * @param <D> The driver type executing the actions.
  */
-public class AddonsHelper extends GenericAddonsHelper {
+public class AddonsHelper<D extends WebDriver> extends GenericAddonsHelper {
     /**
-     * Logger instance.
+     * Driver used in action execution.
      */
-    private static final Logger LOG = LoggerFactory.getLogger(AddonsHelper.class);
-
-    /**
-     * Agent client cached instance.
-     */
-    private final AgentClient agentClient;
+    private final D driver;
 
     /**
      * Initializes a new instance of the helper.
      *
      * @param agentClient Agent client to use for communicating with the Agent.
+     * @param driver Driver.
      */
-    public AddonsHelper(final AgentClient agentClient) {
-        super(null, agentClient);
+    public AddonsHelper(final D driver, final AgentClient agentClient) {
+        super((ReportingDriver) driver, agentClient);
+        this.driver = driver;
+    }
+
+    /**
+     * Runs the action.
+     * @param action Action to run.
+     * @param <T> The driver type that this action supports.
+     * @return True if hte action passed.
+     */
+    public <T extends WebDriver> boolean run(final Action<T> action) {
+        validateAction(action.getClass());
+        return action.run((T) driver);
+    }
+
+    /**
+     * Runs the action.
+     * @param action Action to run.
+     * @param elementSearchCriteria Element search criteria.
+     * @param <T> The driver type that this action supports.
+     * @return True if hte action passed.
+      */
+    public <T extends WebDriver> boolean run(final ElementAction<T> action, final By elementSearchCriteria) {
+        validateAction(action.getClass());
+        return action.run((T) driver, elementSearchCriteria);
+    }
+
+    private void validateAction(final Class<?> actionClass) {
+        ParameterizedType genericInterface = (ParameterizedType) actionClass.getGenericInterfaces()[0];
+        String typeName = genericInterface.getActualTypeArguments()[0].getTypeName()
+                .replaceAll("\\<.*\\>", "");
+        try {
+            if (!Class.forName(typeName).isAssignableFrom(driver.getClass())) {
+                throw new ClassNotFoundException();
+            }
+        } catch (ClassNotFoundException e) {
+            throw new InvalidArgumentException(String.format(
+                    "Driver %s cannot run this action because its driver type must inherit from %s",
+                    driver.getClass().getName(), typeName), e);
+        }
     }
 
     /**
      * Executes an Action using it's proxy.
-     * <p>
      * Addons are tiny automation building blocks that have one or more actions.
-     * Addon Proxy can be obtained from the Addons page at:
-     * <a href="https://app.testproject.io/#/addons">TestProject</a> App.
+     * Addon Proxy can be obtained from the Addons page.
      *
-     * @param action Specific Action proxy.
-     * @return Presumably modified class with updated output fields.
-     */
-    public ActionProxy execute(final ActionProxy action) {
-        return execute(action, -1);
-    }
-
-    /**
-     * Executes an Action using it's proxy.
-     * <p>
-     * Addons are tiny automation building blocks that have one or more actions.
-     * Addon Proxy can be obtained from the Addons page at:
-     * <a href="https://app.testproject.io/#/addons">TestProject</a> App.
-     *
+     * @see <a href="https://app.testproject.io/#/addons">TestProject Addons Page</a>
      * @param action Specific Action proxy.
      * @param by     Element locator in case the Action needs one.
      * @return Presumably modified class with updated output fields.
@@ -84,9 +104,9 @@ public class AddonsHelper extends GenericAddonsHelper {
      * Executes an Action using it's proxy.
      * <p>
      * Addons are tiny automation building blocks that have one or more actions.
-     * Addon Proxy can be obtained from the Addons page at:
-     * <a href="https://app.testproject.io/#/addons">TestProject</a> App.
+     * Addon Proxy can be obtained from the Addons page.
      *
+     * @see <a href="https://app.testproject.io/#/addons">TestProject Addons page</a>.
      * @param action  Specific Action proxy.
      * @param by      Element locator in case the Action needs one.
      * @param timeout maximum amount of time allowed to wait for action execution to complete.
