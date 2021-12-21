fc15f56e00bb76c5f60a08d41cb03c3dcb06dbd8
shai glatshtein <shai.glatshtein@testproject.io>
2021-12-21 10:36:31 +0200

    feat: add new action interfaces Users who will want to create an addon will need to implement one of these interfaces and specify the minimum driver type it supports
    Signed-off-by: shai glatshtein <shai.glatshtein@testproject.io>

---
+++ src/main/java/io/testproject/sdk/internal/addons/interfaces/ElementAction.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2021 TestProject LTD. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.testproject.sdk.internal.addons.interfaces;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Represents an element action that can be uploaded and executed inside a recorder test.
+ * @param <D> Driver type supported by this action.
+ */
+public interface ElementAction<D extends WebDriver> {
+    /**
+     * Implementation of the action code.
+     * @param driver The driver used by the action.
+     * @param elementSearchCriteria Search criteria used to find the element.
+     * @return True if action passed.
+     */
+    boolean run(D driver, By elementSearchCriteria);
+}
